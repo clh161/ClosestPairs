b3cd8a747eb7c994c280a224a9e2d68893817234
Jacob Ho <clh161@gmail.com>
2017-08-20 22:41:59 +0800

    Add minDistance with test

--- src/main/ClosestPairs.java
+++ src/main/ClosestPairs.java
@@ -1,8 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Jacob Ho
  */
 public class ClosestPairs {
+
+    public static double minDistance(List<Coordinate> coordinates) {
+        if (coordinates.size() < 2)
+            return Float.MAX_VALUE;
+        else if (coordinates.size() == 2)
+            return distance(coordinates.get(0), coordinates.get(1));
+        else {
+            coordinates.sort((o1, o2) -> {
+                if (o1.x < o2.x)
+                    return -1;
+                if (o1.x > o2.x)
+                    return 1;
+                return 0;
+            });
+            int mid = coordinates.size() / 2;
+            List<Coordinate> coordinates1 = coordinates.subList(0, mid);
+            List<Coordinate> coordinates2 = coordinates.subList(mid, coordinates.size());
+            double d1 = minDistance(coordinates1);
+            double d2 = minDistance(coordinates2);
+            double minD = Math.min(d1, d2);
+            ArrayList<Coordinate> strips = new ArrayList<>();
+            for (Coordinate coordinate : coordinates)
+                if (Math.abs(coordinates.get(mid).x - coordinate.x) < minD)
+                    strips.add(coordinate);
+            strips.sort((o1, o2) -> {
+                if (o1.y < o2.y)
+                    return -1;
+                if (o1.y > o2.y)
+                    return 1;
+                return 0;
+            });
+            for (int i = 0; i < strips.size(); i++) {
+                for (int j = i + 1; j < strips.size() && strips.get(j).y - strips.get(i).y < minD; j++) {
+                    double d = distance(strips.get(i), strips.get(j));
+                    if (d < minD)
+                        minD = d;
+                }
+            }
+            return minD;
+        }
+    }
+
+    public static double bruteForceMinDistance(List<Coordinate> coordinates) {
+        double min = Float.MAX_VALUE;
+        for (int i = 0; i < coordinates.size(); i++) {
+            for (int j = i + 1; j < coordinates.size(); j++) {
+                Coordinate a = coordinates.get(i);
+                Coordinate b = coordinates.get(j);
+                double distance = distance(a, b);
+                if (distance < min)
+                    min = distance;
+            }
+        }
+        return min;
+    }
+
     public static double distance(Coordinate a, Coordinate b) {
         return Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2));
     }
+
 }
--- src/test/ClosestPairsTest.java
+++ src/test/ClosestPairsTest.java
@@ -1,11 +1,32 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * @author Jacob Ho
  */
 public class ClosestPairsTest {
+    @Test
+    public void minDistance() throws Exception {
+        int loop = 1000;
+        int sampleSize = 1000;
+        int range = 10000;
+        for (int i = 0; i < loop; i++) {
+            ArrayList<Coordinate> coordinates = new ArrayList<>();
+            for (int j = 0; j < sampleSize; j++) {
+                coordinates.add(randomCoordinate(range));
+            }
+            assertEquals(coordinates.toString(), ClosestPairs.bruteForceMinDistance(coordinates), ClosestPairs.minDistance(coordinates), 0);
+        }
+
+    }
+
+    private static Coordinate randomCoordinate(int range) {
+        return new Coordinate(-range + (int) (Math.random() * ((2 * range) + 1)), -range + (int) (Math.random() * ((2 * range) + 1)));
+    }
+
     @Test
     public void distance() throws Exception {
         double approximation = 0.000001;
@@ -17,6 +38,5 @@ public class ClosestPairsTest {
         assertEquals(0, ClosestPairs.distance(new Coordinate(0, 0), new Coordinate(0, 0)), 0);
         assertEquals(26.400758, ClosestPairs.distance(new Coordinate(-7, -4), new Coordinate(17, 7)), approximation);
         assertEquals(14295664.665037, ClosestPairs.distance(new Coordinate(-8975430, 458937), new Coordinate(957293, -9822456)), approximation);
-
     }
 }
